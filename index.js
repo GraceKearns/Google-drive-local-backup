3d69e619487f701b069349783599dc4eecd2ade0
Grace kearns <kearnsgracebusiness@gmail.com>
2023-08-29 16:06:43 +1000

    updated client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const PORT = 3002
       
-  // serve your css as static
+  // serve your css as static 
   app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
