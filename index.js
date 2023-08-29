5e9aaab98ea5b73b51aab2167d21b785c8ce9a27
Grace kearns <kearnsgracebusiness@gmail.com>
2023-08-29 15:50:01 +1000

    pokedex template

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+const PORT = 3002
+      
+  // serve your css as static
+  app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
